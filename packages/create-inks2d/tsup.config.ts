8e666ccd31925788744703af95181473db5a7e04
Phillipe Martins <hi@inkasa.dev>
2023-05-23 16:35:00 -0300

    bump(create-inks2d): v0.0.3

--- packages/create-inks2d/tsup.config.ts
+++ packages/create-inks2d/tsup.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["cjs"],
-	dts: true,
 	minify: true,
-	sourcemap: true,
-	treeshake: true,
 	clean: true,
 });
